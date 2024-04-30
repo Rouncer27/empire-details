e98ce6d648520162be1d24783b6d3fd6270637c4
Trevor Rounce <trevor@switchbackcreative.ca>
2024-04-30 10:33:47 -0600

    added fonts

---
+++ src/data/siteData.js
@@ -0,0 +1,8 @@
+import defaultSrc from "../../public/default.jpg";
+
+export const baseData = {
+  title: "Empire Details",
+  description: "Empire Details - .",
+  siteImg: `https://empire-details.netlify.app/${defaultSrc.src}`,
+  url: "https://empire-details.com/",
+};
