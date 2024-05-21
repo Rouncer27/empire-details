7e0535425ace3f24c3fe9d0c18629dfee2978c69
Trevor Rounce <trevor@switchbackcreative.ca>
2024-05-21 12:55:44 -0600

    added redirects

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -1,8 +1,9 @@
 import defaultSrc from "../../public/default.jpg";
 
 export const baseData = {
-  title: "Empire Details",
-  description: "Empire Details - .",
+  title: "Empire Details - Professional Car Detailing Services",
+  description:
+    "Empire Details - Empire Details offers professional interior car detailing services to give your vehicle a freshen up. Contact us for expert vehicle detailing today.",
   siteImg: `https://empire-details.netlify.app/${defaultSrc.src}`,
   url: "https://empire-details.com/",
 };
